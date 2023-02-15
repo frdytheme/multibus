b56c10e3b2adfb37162b6e0d6b4be71385921060
frdytheme <rbfridayy@gmail.com>
2023-02-15 17:37:26 +0900

    디버깅용 console.log 삭제

--- src/store/fetchRouteSlice.js
+++ src/store/fetchRouteSlice.js
@@ -9,10 +9,8 @@ export const fetchRoute = createAsyncThunk("expRoute/fetchExpRoute", async ({ de
   // 날짜가 오늘이면 현재 시간 이후로 운행하는 경로만 필터링.
   // 날짜가 오늘 이후면 모든 시간 경로 return.
   if (nowTime >= 2300) {
-    console.log("밤 11시 이후니까 내일 것 까지 ");
     return result;
   } else {
-    console.log("현재 시간 이후 예매 가능한 목록 검색 중...");
     const currentRes = result.filter((route) => {
       return route.depPlandTime > depTime;
     });
@@ -29,16 +27,13 @@ export const getExpRoute = createSlice({
     builder
       .addCase(fetchRoute.pending, (state) => {
         state.status = "ready";
-        console.log("출도착지 경로 fetch중...");
       })
       .addCase(fetchRoute.fulfilled, (state, action) => {
         state.status = "success";
         state.data = action.payload;
-        console.log("출도착지 경로 fetch 완료");
       })
       .addCase(fetchRoute.rejected, (state) => {
         state.status = "failed";
-        console.log("출도착지 경로 fetch 실패");
       });
   },
 });
