bb32098999a14dadc36b673fa84d396032ed2c86
rbfridayy <rbfridayy@gmail.com>
2023-02-21 17:17:46 +0900

    라우터 오류 수정/index.js base속성 제거

--- src/index.js
+++ src/index.js
@@ -5,12 +5,11 @@ import { HashRouter } from "react-router-dom";
 import "./asset/style/reset.css";
 import { Provider } from "react-redux";
 import store from "./store/store";
-import { path } from "./asset/DB/requestUrl";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HashRouter basename={path}>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
